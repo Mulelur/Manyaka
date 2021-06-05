2080add172e4c01461ee108a75e50448a388007f
rotonda <rotondwamulelu@gmail.com>
2021-06-05 13:15:37 +0200

    Init

---
+++ src/container/contact/contact.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Contact } from "../../components";
+import { Title } from "../../components/header/styles/header";
+import { LocationIcon, MailIcon, PhoneIcon } from "../../icons/icons";
+
+export default function ContactContainer() {
+  return (
+    <Contact>
+      <Contact.Header>
+        <Contact.Title>Contact Me</Contact.Title>
+        <Contact.Text></Contact.Text>
+      </Contact.Header>
+      <Contact.Body>
+        <Contact.Icons>
+          <Contact.IconsItem>
+            <PhoneIcon />
+            <Contact.IconsTitle>Call Me On</Contact.IconsTitle>
+            <Contact.Text>+27 797651859</Contact.Text>
+          </Contact.IconsItem>
+          <Contact.IconsItem>
+            <MailIcon />
+            <Contact.IconsTitle>Email Me At</Contact.IconsTitle>
+            <Contact.Text email>confidencemanyaka@gmail.com</Contact.Text>
+          </Contact.IconsItem>
+          <Contact.IconsItem>
+            <LocationIcon />
+            <Contact.IconsTitle>Visit Me</Contact.IconsTitle>
+            <Contact.Text>
+              Ga-Manyaka, Stand No 70 ,Burgersfort, 1150
+            </Contact.Text>
+          </Contact.IconsItem>
+        </Contact.Icons>
+      </Contact.Body>
+    </Contact>
+  );
+}
